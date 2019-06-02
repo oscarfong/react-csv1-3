6f87f45be5e7836da2ad34c543d6d8fc9e5f1377
oscarfong <oscar.fong@gmail.com>
2019-06-02 19:33:35 +0800

    2 files modified

--- CSV1.js
+++ CSV1.js
@@ -1,3 +1,34 @@
-import React from 'react';
 
-export default ({ csvLine1 }) => <h1>CSV1 {csvLine1}! </h1>;
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+export default ({ name }) => <h1>Hello {name}! </h1>;
+
+class CSV1 extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: 'React'
+    };
+  }
+
+render() {
+    return (
+      <div>
+        
+        <p>
+          Start editing to see some magic happen :)
+          <p/>
+          Test to see if stackblitz reverts my code.
+        </p>
+
+        <CSV1 csvLine1='1,3,"apple"'>
+        </CSV1>
+
+
+      </div>
+      
+    );
+  }
+}
+
+render(<CSV1 />, document.getElementById('root1'));
